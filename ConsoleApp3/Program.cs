906df03c31f13047fa77f766d67835730866154a
Igor Gaspar <igor.elvis@hotmail.com>
2020-04-06 14:37:28 -0300

    Programa que calcula a area, perimetro e diagonal de um retangulo

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioRetangulo {
+    class Program {
+        static void Main(string[] args) {
+            /* Fazer um programa para ler os valores da largura
+            e altura de um retângulo. Em seguida, mostrar na tela
+            o valor de sua área, perímetro e diagonal. */
+
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+                       
+        }
+    }
+}
